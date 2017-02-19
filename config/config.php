784ac909109637cd396f9dc7cd30b7403c78fd42
apirat_BSRU <ikool009@hotmail.com>
2017-02-19 14:27:02 +0700

    edit for server dynamic Domain OK check error OK

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php 
+	$host = "localhost";
+	$user = "bsruteam_app";
+	$pass = "XPjHGkHcB7";
+	$DABASE = "bsruteam_student";
+
+	$obj_con = mysqli_connect($host, $user, $pass,$DABASE) or dir(mysqli_error($obj_con));
+
+?>
--- service/createUser.php
+++ service/createUser.php
@@ -1,16 +1,24 @@
 <?php
 include "../lib/phpqrcode/qrlib.php";
-	
+include '../config/config.php';
+
+// "studentCode=" 
+//  "username=" 
+//  "password=" 
+//  "BookCode=" 
+//  "Domain=" 
+
+
+    
 if(count($_REQUEST)>0){
 
-	$box = array();
-	$json = array();
-	$tempDir = "../tempQRcode/"; 
-	$tempQrRequest = $_REQUEST["Domain"];
-	$tempQRSERVER = $tempQrRequest."tempQRcode/";
-
-	$codeContents = $_REQUEST["studentCode"]; 
-	//$codeContents = 555; 
+    $box = array();
+    $json = array();
+    $tempDir = "../tempQRcode/"; 
+    $tempQrRequest = $_REQUEST["Domain"];
+    $tempQRSERVER = $tempQrRequest."tempQRcode/";
+    $codeContents = $_REQUEST["studentCode"]; 
+    //$codeContents = 555; 
      
     // we need to generate filename somehow,  
     // with md5 or with database ID used to obtains $codeContents... 
@@ -21,23 +29,45 @@ if(count($_REQUEST)>0){
      
     // generating 
     if (!file_exists($pngAbsoluteFilePath)) { 
-         QRcode::png($codeContents, $pngAbsoluteFilePath); 
-        $json["status"] = true;
-        $json["pathQR"] = $tempQRSERVER.$fileName;
-        //echo $json["pathQR"] ;
+        
+       
+        $sql_check_username = "SELECT * FROM `stadent_info` WHERE `username` = '".$_REQUEST["username"]."' OR `student_code` = '".$_REQUEST["studentCode"]."' ;";
+        
+        if ($res = mysqli_query($obj_con, $sql_check_username)) {
+            if(mysqli_num_rows($res)>0){
+                $json["status"] = false;
+                $json["pathQR"] = "";
+                $json["massage"] = "Users are already";
+            }else{
+                 QRcode::png($codeContents, $pngAbsoluteFilePath); 
+                $json["status"] = true;
+                $json["pathQR"] = $tempQRSERVER.$fileName;
+                $json["massage"] = "OK";
+            }
+            //echo $json["pathQR"] ;
+            
+        }else{
+                $json["status"] = false;
+                $json["pathQR"] = "";
+                $json["massage"] = $sql_check_username." ===>".mysqli_error($obj_con);
+        }
+
         array_push($box, $json);
         echo json_encode($box);
+
     } else { 
         $json["status"] = false;
         $json["pathQR"] = "";
+        $json["massage"] = "user error";
         array_push($box, $json);
         echo json_encode($box);
     } 
 }else{
-	// no value
+    // no value
+
 
 }
-	
+    
      
    
 ?>
